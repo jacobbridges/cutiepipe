d206e6bf3b0e6af208982c79b71194d3604a999d
jacobbridges <him@jacobandkate143.com>
2015-07-15 23:51:55 -0400

    git init

---
+++ cutiepipe.py
@@ -0,0 +1,19 @@
+"""
+cutiepipe
+
+Utility providing a simple interface for named Unix pipes.
+
+Example Usage:
+ # Terminal 1
+ $ cat data.csv | cutiepipe
+
+ # Terminal 2
+ $ cutiepipe | data_copy.csv
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
