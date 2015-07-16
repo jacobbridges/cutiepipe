dd08ce36e17c8a7f53aca755bff849ed52d66374
jacobbridges <him@jacobandkate143.com>
2015-07-16 00:38:34 -0400

    added simple implementation with beanstalkd

--- cutiepipe.py
+++ cutiepipe.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 cutiepipe
 
@@ -10,9 +11,45 @@ Example Usage:
  # Terminal 2
  $ cutiepipe | data_copy.csv
 """
+import sys
+import select
+import beanstalkc
+
 
 def main():
-    pass
+
+    # Attempt to create a localhost connection with beanstalkd
+    try:
+        beanstalk = beanstalkc.Connection()
+    except beanstalkc.SocketError:
+        print 'Could not find beanstalkd connection.'
+        exit()
+    except Exception as e:
+        raise e
+
+    # Create a cleanup function
+    def fin():
+        beanstalk.close()
+
+    # Wait for data to read and redirect
+    while True:
+
+        # Check for a beanstalk job
+        job = beanstalk.reserve(timeout=0)
+
+        # Process job
+        if job is not None:
+            sys.stdout.write(job.body)
+            sys.stdout.write('\n')
+            job.delete()
+            fin()
+            break
+
+        # Check for data in the stdin
+        if select.select([sys.stdin], [], [], 0.0)[0]:
+            beanstalk.put(sys.stdin.read())
+            fin()
+            break
 
 
 if __name__ == '__main__':
