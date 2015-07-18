d42253f3f150d5b6b1f9ed9a1816f2576f57b41e
jacobbridges <him@jacobandkate143.com>
2015-07-18 14:46:18 -0400

    now "output" pipes will listen for an "input" pipe

--- cutiepipe.py
+++ cutiepipe.py
@@ -5,17 +5,13 @@ cutiepipe
 
 Utility providing a simple interface for named Unix pipes.
 
-Example Usage:
- # Terminal 1
- $ cat data.csv | cutiepipe
-
- # Terminal 2
- $ cutiepipe | data_copy.csv
+See project README for example usage.
 """
 
 import sys
 import select
 import socket
+from time import sleep
 
 HOST = 'localhost'
 PORT = 10000
@@ -26,63 +22,85 @@ def throw_error(message):
     print >> sys.stderr, "Cutie Pipe: " + message
 
 
-def write_to_sock(e=None):
+def stream_to_socket():
+    # Create a simple socket server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(CNXN)
+    s.listen(1)
 
-    # Is there data in the stdin?
-    if select.select([sys.stdin], [], [], 0.0)[0]:
-
-        # Create a simple socket server
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(CNXN)
-        s.listen(1)
-
+    try:
         # Accept incoming connections
-        try:
-            client, addr = s.accept()
-        except KeyboardInterrupt:
-            exit()
+        client, addr = s.accept()
 
         # Stream data from stdin to the client
-        try:
-            for line in iter(sys.stdin.readline, ''):
-                client.send(line)
-        except KeyboardInterrupt:
-            exit()
+        for line in iter(sys.stdin.readline, ''):
+            client.send(line)
 
         # Finally, close the client
         client.close()
-
-    # If no data is in the stdin
-    else:
-        if e.__repr__ and 'Connection refused' in e.__repr__():
-            throw_error("No input pipe detected")
-            exit()
-        throw_error("No input data detected and socket error " + e.__repr__())
+    except KeyboardInterrupt:
+        exit()
 
 
-def read_from_sock(s):
-
+def read_from_socket(s):
     def get_data():
         return s.recv(32)
 
     # Start data retrieval until no more data is available (or connection closes)
-    for packet in iter(get_data, ''):
-        sys.stdout.write(packet)
-        sys.stdout.flush()
+    try:
+        for packet in iter(get_data, ''):
+            sys.stdout.write(packet)
+            sys.stdout.flush()
+    except KeyboardInterrupt:
+        exit()
+    finally:
+        s.close()
+
+
+def create_listener():
+
+    # Loop until socket connection can be established
+    while True:
+
+        # Create a socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+
+            # Try connecting input pipe
+            s.connect(CNXN)
+
+        except socket.error:
+
+            # If connection failed, try again in a second
+            s.close()
+            sleep(1)
+            continue
+
+        except KeyboardInterrupt:
+
+            # If a Ctrl-C is encountered, close socket and exit program
+            s.close()
+            exit()
+
+        else:
+
+            # If the connection was successful, return the connected socket
+            return s
 
 
 def main():
+    # Is there data in stdin?
+    if select.select([sys.stdin], [], [], 0.0)[0]:
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        sock.connect(CNXN)
-    except socket.error as e:
-        write_to_sock(e)
+        # Stream the data through a socket
+        stream_to_socket()
+
+    # No data in stdin?
     else:
-        read_from_sock(sock)
-    finally:
-        sock.close()
 
+        # Setup a socket listener
+        read_from_socket(create_listener())
 
 
 if __name__ == '__main__':
